8fcb84942980e1104f953d5fd4708409b419d099
Buck Shlegeris <bshlegeris@gmail.com>
2013-12-07 19:22:16 -0800

    initial commit

---
+++ lib/active_record_lite/test.rb
@@ -0,0 +1,36 @@
+require_relative './associatable'
+require_relative './db_connection' # use DBConnection.execute freely here.
+require_relative './mass_object'
+require_relative './searchable'
+require_relative './sql_object'
+require 'active_support/inflector'
+
+cats_db_file_name =
+  File.expand_path(File.join(File.dirname(__FILE__), "../../test/cats.db"))
+DBConnection.open(cats_db_file_name)
+
+class Cat < SQLObject
+  set_table_name("cats")
+  my_attr_accessible(:id, :name, :owner_id)
+
+  belongs_to :human, :class_name => "Human", :primary_key => :id, :foreign_key => :owner_id
+  has_one_through :house, :human, :house
+end
+
+class Human < SQLObject
+  set_table_name("humans")
+  my_attr_accessible(:id, :fname, :lname, :house_id)
+
+  has_many :cats, :foreign_key => :owner_id
+  belongs_to :house
+end
+
+class House < SQLObject
+  set_table_name("houses")
+  my_attr_accessible(:id, :address, :house_id)
+end
+
+
+cat = Cat.find(1)
+
+p cat.human
